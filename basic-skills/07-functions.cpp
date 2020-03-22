64246882fce201a2adbeb03a30e6a44efeae3f33
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-03-22 17:46:25 -0500

    Functions 1 on C++

---
+++ basic-skills/07-functions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void printCuteMessage()
+{
+    cout << "Eres increible" << endl;
+    cout << ":D!!" << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    printCuteMessage();
+    return 0;
+}
