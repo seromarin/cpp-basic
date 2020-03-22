86af103729286e7172b60d684c8c313d910f0893
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-03-22 15:59:01 -0500

    Operators in C++

---
+++ basic-skills/03-operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n1 = 10;
+    int n2 = 20;
+
+    cout << n1 + n2 << endl;
+    cout << n1 - n2 << endl;
+    cout << n1 * n2 << endl;
+    cout << n1 / n2 << endl;
+    cout << n1 % n2 << endl;
+
+    return 0;
+}
