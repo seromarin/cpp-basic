2ff98ece2c6b071627fc54073a3ba655ee83100a
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-03-22 15:43:05 -0500

    Data types in C++

---
+++ basic-skills/01-data-types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "Tipos de datos en C++" << endl;
+    cout << 10 << endl;
+    cout << 3.1416f << endl;
+    cout << true << endl;
+    cout << false << endl;
+    cout << 's' << endl;
+    return 0;
+}
