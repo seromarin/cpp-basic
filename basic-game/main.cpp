bdac3d1327642f6ea944d8c0d7c53d8212ea1d3f
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-03-22 19:06:13 -0500

    Basic movement implementation

--- basic-game/main.cpp
+++ basic-game/main.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+enum USER_INPUT {
+    RIGHT,
+    LEFT,
+    QUIT,
+    NOTHING,
+};
+
 void drawMap(int heroPosition, char gameMap[5]) {
     for (size_t i = 0; i < sizeof(gameMap); i++)
     {
@@ -14,15 +21,47 @@ void drawMap(int heroPosition, char gameMap[5]) {
     }
 }
 
+int checkHeroPosition(int heroPosition, USER_INPUT heroMovement) {
+    switch (heroMovement)
+    {
+    case RIGHT:
+        return heroPosition < 4 ? heroPosition + 1 : heroPosition;
+        break;
+    case LEFT:
+        return heroPosition > 0 ? heroPosition - 1 : heroPosition;
+        break;
+    case NOTHING:
+        return heroPosition;
+        break;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     int heroPosition = 0;
+    char userInput = ' ';
+    USER_INPUT heroMovement;
+    bool gameOver = false;
     char gameMap[5] = {'I','I','I','I','I'};
 
-    drawMap(heroPosition, gameMap);
-    cout << endl;
-    system("clear");
-    drawMap(heroPosition, gameMap);
+    cout << "Welcome to the game" << endl;
+
+    while(!gameOver) {
+        cout << "Move right: 'd' - Move left: 'a' - Quit: 'q'" << endl;
+        drawMap(heroPosition, gameMap);
+        // cout << endl;
+        cin >> userInput;
+        heroMovement = userInput == 'd' ? RIGHT : userInput == 'a' ? LEFT : userInput == 'q' ? QUIT : NOTHING;
+        if (heroMovement == QUIT) {
+            gameOver = true;
+        } else {
+            heroPosition = checkHeroPosition(heroPosition, heroMovement);
+            system("clear");
+            drawMap(heroPosition, gameMap);
+        }
+        // cout << endl;
+    }
+
 
 
     return 0;
